85b96853b86841d8147127361cda1da268856584
s336154 <sarisiraj@gmail.com>
2023-09-17 11:23:19 +0200

    Commit 25

--- app/src/main/java/com/example/s336154_mappe1/OmspillAktivitet.java
+++ app/src/main/java/com/example/s336154_mappe1/OmspillAktivitet.java
@@ -1,21 +1,17 @@
 package com.example.s336154_mappe1;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
 
 public class OmspillAktivitet extends AppCompatActivity {
-
-
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.om_spill);
+
         Log.d("I om spill ", "oncreate Om Spill Aktivitet");
         Button omspill_knapp1=findViewById(R.id.omspill_knapp1);
         omspill_knapp1.setOnClickListener(new View.OnClickListener() {
