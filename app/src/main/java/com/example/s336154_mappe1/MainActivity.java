6a60c5cde87f16242c7723582c893a468e32e524
s336154 <sarisiraj@gmail.com>
2023-09-17 11:21:01 +0200

    Commit 24

--- app/src/main/java/com/example/s336154_mappe1/MainActivity.java
+++ app/src/main/java/com/example/s336154_mappe1/MainActivity.java
@@ -1,21 +1,13 @@
 package com.example.s336154_mappe1;
 
-import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
-
-import android.app.Dialog;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-
 
 public class MainActivity extends AppCompatActivity implements
         MinDialog.MittInterface{
@@ -70,13 +62,6 @@ public class MainActivity extends AppCompatActivity implements
             } });
 
 
-        Button dialogknapp = findViewById(R.id.Avslutt);
-        dialogknapp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                visDialog(view);}
-        });
-
         Button preferanseknapp = findViewById(R.id.preferanser);
         Intent intent = new Intent(this, SettingsActivity.class);
         preferanseknapp.setOnClickListener(new View.OnClickListener() {
@@ -84,4 +69,12 @@ public class MainActivity extends AppCompatActivity implements
             public void onClick(View view) {
                 startActivity(intent); }
         });
-    } }
+
+        Button dialogknapp = findViewById(R.id.Avslutt);
+        dialogknapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                visDialog(view);}
+        });
+    }
+}
